f68196c48b34ca70cde62778e1e3b0441231201c
Jeff Fraser <sixfeetover@gmail.com>
2017-02-23 14:25:04 -0500

    Linting

--- source-sans-pro-rails.gemspec
+++ source-sans-pro-rails.gemspec
@@ -4,23 +4,23 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'source-sans-pro/rails/version'
 
 Gem::Specification.new do |spec|
-  spec.name          = "source-sans-pro-rails"
+  spec.name          = 'source-sans-pro-rails'
   spec.version       = SourceSansPro::Rails::VERSION
-  spec.authors       = ["Jeff Fraser"]
-  spec.email         = ["jfraser@breuer.com"]
-  spec.description   = %q{Add Adobe's Source Sans Pro Font to your Rails app through the asset pipeline}
-  spec.summary       = %q{The Adobe Source Sans Pro font in the asset pipeline}
-  spec.homepage      = "https://github.com/veracross/source-sans-pro-rails"
-  spec.licenses      = ["MIT", "SIL Open Font License"]
+  spec.authors       = ['Jeff Fraser']
+  spec.email         = ['jfraser@breuer.com']
+  spec.description   = "Add Adobe's Source Sans Pro Font to your Rails app through the asset pipeline"
+  spec.summary       = 'The Adobe Source Sans Pro font in the asset pipeline'
+  spec.homepage      = 'https://github.com/veracross/source-sans-pro-rails'
+  spec.licenses      = ['MIT', 'SIL Open Font License']
 
-  spec.files         = `git ls-files`.split($/)
+  spec.files         = `git ls-files`.split($INPUT_RECORD_SEPARATOR)
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_dependency "railties", ">= 3.2"
+  spec.add_dependency 'railties', '>= 3.2'
 
-  spec.add_development_dependency "bundler", "~> 1.3"
-  spec.add_development_dependency "rake"
-  spec.add_development_dependency "sass-rails"
-  spec.add_development_dependency "tzinfo"
+  spec.add_development_dependency 'bundler', '~> 1.3'
+  spec.add_development_dependency 'rake'
+  spec.add_development_dependency 'sass-rails'
+  spec.add_development_dependency 'tzinfo'
 end
--- test/source_sans_pro_test.rb
+++ test/source_sans_pro_test.rb
@@ -3,52 +3,52 @@ require 'test_helper'
 class SourceSansProTest < ActionDispatch::IntegrationTest
   teardown { clean_sprockets_cache }
 
-  test "engine is loaded" do
+  test 'engine is loaded' do
     assert_equal ::Rails::Engine, SourceSansPro::Rails::Engine.superclass
   end
 
-  test "fonts are served" do
-    get "/assets/sourcesanspro-regular-webfont.eot"
+  test 'fonts are served' do
+    get '/assets/sourcesanspro-regular-webfont.eot'
     assert_response :success
 
-    get "/assets/sourcesanspro-regular-webfont.ttf"
+    get '/assets/sourcesanspro-regular-webfont.ttf'
     assert_response :success
 
-    get "/assets/sourcesanspro-regular-webfont.woff"
+    get '/assets/sourcesanspro-regular-webfont.woff'
     assert_response :success
 
-    get "/assets/sourcesanspro-regular-webfont.svg"
+    get '/assets/sourcesanspro-regular-webfont.svg'
     assert_response :success
   end
 
-  test "stylesheets are served" do
-    get "/assets/source-sans-pro.css"
+  test 'stylesheets are served' do
+    get '/assets/source-sans-pro.css'
     assert_source_sans_pro(response)
 
-    %w{black bold extralight italic light regular semibold}.each do |font_weight|
+    %w(black bold extralight italic light regular semibold).each do |font_weight|
       get "/assets/source-sans-pro/#{font_weight}.css"
       assert_source_sans_pro(response)
     end
   end
 
-  test "stylesheets contain asset pipeline references to fonts" do
-    get "/assets/source-sans-pro.css"
-    assert_match "/assets/sourcesanspro-regular-webfont.eot",  response.body
-    assert_match "/assets/sourcesanspro-regular-webfont.eot?#iefix", response.body
-    assert_match "/assets/sourcesanspro-regular-webfont.woff", response.body
-    assert_match "/assets/sourcesanspro-regular-webfont.ttf",  response.body
-    assert_match "/assets/sourcesanspro-regular-webfont.svg#sourcesanspro-regular", response.body
+  test 'stylesheets contain asset pipeline references to fonts' do
+    get '/assets/source-sans-pro.css'
+    assert_match '/assets/sourcesanspro-regular-webfont.eot', response.body
+    assert_match '/assets/sourcesanspro-regular-webfont.eot?#iefix', response.body
+    assert_match '/assets/sourcesanspro-regular-webfont.woff', response.body
+    assert_match '/assets/sourcesanspro-regular-webfont.ttf',  response.body
+    assert_match '/assets/sourcesanspro-regular-webfont.svg#sourcesanspro-regular', response.body
   end
 
-  test "stylesheet is available in a css sprockets require" do
-    get "/assets/sprockets-require.css"
+  test 'stylesheet is available in a css sprockets require' do
+    get '/assets/sprockets-require.css'
     assert_source_sans_pro(response)
   end
 
   private
 
   def clean_sprockets_cache
-    FileUtils.rm_rf File.expand_path("../dummy/tmp",  __FILE__)
+    FileUtils.rm_rf File.expand_path('../dummy/tmp', __FILE__)
   end
 
   def assert_source_sans_pro(response)
